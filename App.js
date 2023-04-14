3ab718dfe91e4d8f6325177bf5bd5099ea5fcac5
betzeafer <santiagoheerrera@gmail.com>
2023-04-13 21:02:09 -0500

    user

--- App.js
+++ App.js
@@ -1,12 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
+import { useForm, Controller } from "react-hook-form";
+import { Button,TextInput } from 'react-native-paper';
 
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+      <Stack.Screen name="Login" component={LoginScreen} options={{title:'Inicio'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+function LoginScreen({navigation}){
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {
+      firstName: '',
+      lastName: ''
+    }
+  });
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+       <Controller
+        control={control}
+        rules={{
+         required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label="Usuario"
+            onBlur={onBlur}
+            mode='outlined'
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="firstName"
+      />
+      {errors.firstName && <Text>This is required.</Text>}
+      <Controller
+        control={control}
+        rules={{
+         required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            label="Contraseña"
+            onBlur={onBlur}
+            
+            mode='outlined'
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="firstName"
+      />
+      {errors.firstName && <Text>This is required.</Text>}
+
+      <Button
+      style={{marginTop:15}}
+      icon="send"
+      mode='contained'
+      onPress={() =>{
+      navigation.navigate('Usuario')
+      }}
+
+      >Ingresar</Button>
+       </View>
   );
 }
 
